812384c0e8fffaec295402db48d92c209110d3d5
willykar <wilsonnjenga799@gmail.com>
2023-09-07 03:10:16 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints "Programming is like building a multilingual puzzle",
+ *  followed by a new line
+ *  Return: 0 success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
