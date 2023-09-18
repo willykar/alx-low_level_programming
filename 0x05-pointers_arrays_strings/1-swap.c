793b84c0c4e46742a5a67b68d62531a76e023e60
willykar <103494595+willykar@users.noreply.github.com>
2023-09-18 22:05:42 +0300

    Update 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers
+ * @a: first pointer to an int
+ * @b: second pointer to an int
+*/
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
