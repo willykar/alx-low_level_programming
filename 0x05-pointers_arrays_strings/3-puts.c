be73a7f40ea642cb0e9d8adc047b18cb8c590c2a
willykar <103494595+willykar@users.noreply.github.com>
2023-09-18 22:10:50 +0300

    Update 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - prints a string followed by a new line to stdout
+ * @str: pointer to a string
+*/
+
+void _puts(char *str)
+{
+	int a;
+
+	for (int a = 0; str[a] != '\0'; a++)
+	{
+		putchar(str[a]);
+	}
+	putchar('\n');
+}
