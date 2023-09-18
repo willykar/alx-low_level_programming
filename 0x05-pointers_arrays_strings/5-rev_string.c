7f2557c95ae89446d1d71075fbe67949c7568416
willykar <103494595+willykar@users.noreply.github.com>
2023-09-18 22:15:34 +0300

    Update 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: pointer to the string to be reversed
+*/
+
+void rev_string(char *s)
+{
+	int length = 0;
+
+	int a;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	for (a = 0; a < length / 2; a++)
+	{
+		char temp = s[a];
+
+		s[a] = s[length - a - 1];
+		s[length - a - 1] = temp;
+	}
+}
