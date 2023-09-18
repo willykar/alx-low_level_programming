4d4c1dcbfacd481fa29cd60d11346fcc500cd346
willykar <wilsonnjenga799@gmail.com>
2023-09-18 15:28:45 -0700

    update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,7 @@
 */
 int _atoi(char *s)
 {
-	int m, n, o, length, x, numb;
+	int m, n, o, length, x, numb
 
 	m = 0;
 	n = 0;
@@ -15,26 +15,27 @@ int _atoi(char *s)
 	length = 0;
 	x = 0;
 	numb = 0;
-  
+
 	while (s[length] != '\0')
 		length++;
-			while (m < length && x == 0)
-			{
-				if (s[m] == '-')
-					++n;
-				if (s[m] >= '0' && s[m] <= '9')
-				{
-					numb = s[m] - '0';
-					if (n % 2)
-						numb = -numb;
-					o = o * 10 + numb;
-					x = 1;
-					if (s[m + 1] < '0' || s[m + 1] > '9')
-						break;
-					x = 0;
-				}
-				m++;
-			}
+	while (m < length && x == 0)
+	{
+		if (s[m] == '-')
+			++n;
+		if (s[m] >= '0' && s[m] <= '9')
+		{
+			numb = s[m] - '0';
+			if (n % 2)
+				numb = -numb;
+			o = o * 10 + numb;
+			x = 1;
+			if (s[m + 1] < '0' || s[m + 1] > '9')
+				break;
+			x = 0;
+		}
+
+		m++;
+	}
 	if (x == 0)
 		return (0);
 	return (o);
