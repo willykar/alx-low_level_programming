b4fd34e41355ca65b5f196e1cb2a04e00b6ecf8d
willykar <wilsonnjenga799@gmail.com>
2023-09-21 12:25:30 -0700

    update

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -15,9 +15,9 @@ void print_number(int n)
 
 	x = n;
 
-	if (n / 10 != 0)
+	if (x / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(x / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar(x % 10 + '0');
 }
