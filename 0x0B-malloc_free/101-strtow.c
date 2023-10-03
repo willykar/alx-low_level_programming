20f4f9355f9e5d27b3f1ba591a61eb789ade1b1a
willykar <103494595+willykar@users.noreply.github.com>
2023-10-03 13:12:08 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,99 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "main.h"
 
-int countWords(const char *str);
+/**
+ * alloc - allocates space in memory for an array
+ * @str: string
+ * @len: length of string
+ * @size: size of array
+ *
+ * Return: a pointer to the array or null if allocation was unsucessful
+ */
+char **alloc(char *str, int len, int size)
+{
+	int i, j, wide;
+	char **a, before;
 
+	a = malloc((size + 1) * sizeof(char *));
+	before = ' ';
+
+	for (i = 0; i < size; i++)
+	{
+		while (j < len)
+		{
+			if (str[j] == ' ' && before != ' ')
+			{
+				before = ' ';
+				j++;
+				break;
+			}
+			if (str[j] != ' ')
+				wide++;
+			before = str[j];
+			j++;
+		}
+
+		a[i] = malloc((wide + 1) * sizeof(char));
+		if (a[i] == NULL)
+		{
+			return (NULL);
+		}
+		wide = 0;
+	}
+	a[size] = NULL;
+
+	return (a);
+}
+
+/**
+ * strtow - splits a string into words
+ * @str: the string to split
+ *
+ * Return: a pointer to an array of strings or NULL if error
+ */
 char **strtow(char *str)
 {
-    int wordIndex = 0;
-    int start = 0;
-    int i, j;
-    int numWords = countWords(str);
-    
-    char **wordArray = (char **)malloc((numWords + 1) * sizeof(char *));
-    
-    if (str == NULL || *str == '\0') {
-        return (NULL);
-    }
+	char **a, before = ' ';
+	int i, j = 0, k = 0, c = 0, len, size = 0;
 
-    numWords = countWords(str);
-    
-    if (wordArray == NULL) {
-        return (NULL);
-    }
-
-    for (i = 0; str[i] != '\0'; i++) {
-        if (str[i] == ' ') {
-            if (i > start) {
-                wordArray[wordIndex] = (char *)malloc((i - start + 1) * sizeof(char));
-                if (wordArray[wordIndex] == NULL)
-                {
-                    for (j = 0; j < wordIndex; j++) {
-                        free(wordArray[j]);
-                    }
-                    free(wordArray);
-                    return (NULL);
-                }
-                strncpy(wordArray[wordIndex], str + start, i - start);
-                wordArray[wordIndex][i - start] = '\0';
-                wordIndex++;
-            }
-            start = i + 1;
-        }
-    }
-    if (str[start] != '\0') {
-        wordArray[wordIndex] = (char *)malloc((strlen(str + start) + 1) * sizeof(char));
-        if (wordArray[wordIndex] == NULL)
-        {
-            for (j = 0; j < wordIndex; j++) {
-                free(wordArray[j]);
-            }
-            free(wordArray);
-            return (NULL);
-        }
-        strcpy(wordArray[wordIndex], str + start);
-        wordIndex++;
-    }
-
-    wordArray[wordIndex] = NULL;
-    return (wordArray);
-}
-
-int countWords(const char *str)
-{
-    int count = 0;
-    int word = 0;
-    int i;
-
-    for (i = 0; str[i] != '\0'; i++) {
-        if (str[i] != ' ') {
-            if (!word) {
-                count++;
-                word = 1;
-            }
-        } else {
-            word = 0;
-        }
-    }
-
-    return (count);
-}
-
-int main(void)
-{
-    int i;
-    char **words = strtow("Hello World   This is a test");
-    if (words == NULL) {
-        printf("Memory allocation failed or input is empty.\n");
-        return (1);
-    }
-
-    for (i = 0; words[i] != NULL; i++) {
-        printf("%s\n", words[i]);
-        free(words[i]);
-    }
-
-    free(words);
-    return (0);
+	if (str == NULL || strlen(str) == 0)
+		return (NULL);
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+	{
+		if (str[i] != ' ')
+			c = 1;
+		if (str[i] != ' ' && before == ' ')
+			size++;
+		before = str[i];
+	}
+	a = alloc(str, len, size);
+	if (a == NULL || c == 0)
+		return (NULL);
+	before = ' ';
+	for (i = 0; i < size; i++)
+	{
+		while (j < len)
+		{
+			if (str[j] == ' ' && before != ' ')
+			{
+				before = ' ';
+				j++;
+				break;
+			}
+			if (str[j] != ' ')
+			{
+				a[i][k] = str[j];
+				k++;
+			}
+			before = str[j];
+			j++;
+		}
+		a[i][k] = '\0';
+		k = 0;
+	}
+	return (a);
 }
