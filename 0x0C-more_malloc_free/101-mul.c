3c56b0cb7288b1d152d97ed6bf07b8a4668da46a
willykar <wilsonnjenga799@gmail.com>
2023-10-05 10:05:14 -0700

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,45 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-unsigned long multiply(unsigned long num1, unsigned long num2)
-{
-	return num1 * num2;
+#include "main.h"
+int isPositiveInteger(const char *str) {
+    while (*str) {
+        if (*str < '0' || *str > '9') {
+            return (0);
+        }
+        str++;
+    }
+    return (1);
 }
-int isNumeric(const char *str)
-{
-	while (*str)
-	{
-		if (!isdigit(*str))
-		{
-			return (0);
-		}
-		str++;
-	}
-	return (1);
+int multiply(int num1, int num2) {
+    return (num1 * num2);
 }
+
 int main(int argc, char *argv[])
 {
-	char *num1_str, *num2_str;
-	 unsigned long num1, num2, result;
+	int num1, num2, result;
+    if (argc != 3) {
+        printf("Error\n");
+        return (98);
+    }
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (98);
-	}
-	num1_str = argv[1];
-	num2_str = argv[2];
+    if (!isPositiveInteger(argv[1]) || !isPositiveInteger(argv[2])) {
+       printf("Error\n");
+        return (98);
+    }
 
-	if (!isNumeric(num1_str) || !isNumeric(num2_str))
-	{
-		printf("Error\n");
-		return (98);
-	}
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
 
-	num1 = strtoull(num1_str, NULL, 10);
-	num2 = strtoull(num2_str, NULL, 10);
-	result = multiply(num1, num2);
+    result = multiply(num1, num2);
 
-	printf("%lu\n", result);
-	return (0);
+    printf("%d\n", result);
+
+    return (0);
 }
