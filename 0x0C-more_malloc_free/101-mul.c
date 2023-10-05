f1119f5010ac146ec4be6e7e0151e66f122ee11b
willykar <wilsonnjenga799@gmail.com>
2023-10-05 10:36:27 -0700

    update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,76 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
-/**
- * _puts - prints out a string that is followed by a new line
- * @str: pointer to a string
- * Return: Nothing
-*/
-void _puts(char *str)
+#include <ctype.h>
+unsigned long multiply(unsigned long num1, unsigned long num2)
 {
-	int i = 0;
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
+        return num1 * num2;
 }
-/**
- * _atoi - converts string to an integer
- * @s: pointer to string
- * Return: integer
- */
-int _atoi(const char *s)
+int isNumeric(const char *str)
 {
-	int si = 1;
-	unsigned long int result = 0, firstNum, a;
+        while (*str)
+        {
+                if (!isdigit(*str))
+                {
+                        return (0);
+                }
+                str++;
+        }
+        return (1);
+}
+int main(int argc, char *argv[])
+{
+        char *num1_str, *num2_str;
+         unsigned long num1, num2, result;
 
-	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
-	{
-		if (s[firstNum] == '-')
-		{
-			si *= -1;
-		}
-	}
-	for (a= firstNum; s[a] >= 48 && s[a] <= 57; a++)
-	{
-		result *= 10;
-		result += (s[a] - 48);
-	}
-	return (si * result);
-}
-/**
- * print_int - prints an integer.
- * @n: int
- * Return: 0
- */
-void print_int(unsigned long int n)
-{
-	unsigned long int divisor = 1, i, r;
-	for (i = 0; n / divisor > 9; i++, divisor *= 10)
-		;
-	for (; divisor >= 1; n %= divisor, divisor /= 10)
-	{
-		r = n / divisor;
-		_putchar('0' + r);
-	}
-}
-/**
- * main - prints the result of multiplication
- * @argc: int
- * @argv: list of arguments
- * Return: 0
- */
-int main(int argc, char const *argv[])
-{
-	(void)argc;
+        if (argc != 3)
+        {
+                printf("Error\n");
+                return (98);
+        }
+        num1_str = argv[1];
+        num2_str = argv[2];
 
-	if (argc != 3)
-	{
-		_puts("Error ");
-		exit(98);
-	}
-	print_int(_atoi(argv[1]) * _atoi(argv[2]));
-	_putchar('\n');
-	return (0);
+        if (!isNumeric(num1_str) || !isNumeric(num2_str))
+        {
+                printf("Error\n");
+                return (98);
+        }
+
+        num1 = strtoull(num1_str, NULL, 10);
+        num2 = strtoull(num2_str, NULL, 10);
+        result = multiply(num1, num2);
+
+        printf("%lu\n", result);
+        return (0);
 }
