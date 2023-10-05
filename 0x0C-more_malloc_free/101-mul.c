f70175a261f968374824a2d45d2e8857b7fccf18
willykar <wilsonnjenga799@gmail.com>
2023-10-05 10:42:54 -0700

    update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,43 +3,41 @@
 #include <ctype.h>
 unsigned long multiply(unsigned long num1, unsigned long num2)
 {
-        return num1 * num2;
+	return num1 * num2;
 }
 int isNumeric(const char *str)
 {
-        while (*str)
-        {
-                if (!isdigit(*str))
-                {
-                        return (0);
-                }
-                str++;
-        }
-        return (1);
+	while (*str)
+	{
+		if (!isdigit(*str))
+		{
+			return (0);
+		}
+		str++;
+	}
+	return (1);
 }
 int main(int argc, char *argv[])
 {
-        char *num1_str, *num2_str;
-         unsigned long num1, num2, result;
+	char *num1_str, *num2_str;
+	unsigned long num1, num2, result;
 
-        if (argc != 3)
-        {
-                printf("Error\n");
-                return (98);
-        }
-        num1_str = argv[1];
-        num2_str = argv[2];
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	num1_str = argv[1];
+	num2_str = argv[2];
 
-        if (!isNumeric(num1_str) || !isNumeric(num2_str))
-        {
-                printf("Error\n");
-                return (98);
-        }
-
-        num1 = strtoull(num1_str, NULL, 10);
-        num2 = strtoull(num2_str, NULL, 10);
-        result = multiply(num1, num2);
-
-        printf("%lu\n", result);
-        return (0);
+	if (!isNumeric(num1_str) || !isNumeric(num2_str))
+	{
+		printf("Error\n");
+		return (98);
+	}
+	num1 = strtoull(num1_str, NULL, 10);
+	num2 = strtoull(num2_str, NULL, 10);
+	result = multiply(num1, num2);
+	printf("%lu\n", result);
+	return (0);
 }
