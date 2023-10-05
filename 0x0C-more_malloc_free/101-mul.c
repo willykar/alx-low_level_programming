66f28b8cd99f71a1ce5e9aaaac366fa980ed856c
willykar <wilsonnjenga799@gmail.com>
2023-10-05 10:32:11 -0700

    update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,38 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-int isPositiveInteger(const char *str) {
-    while (*str) {
-        if (*str < '0' || *str > '9') {
-            return (0);
-        }
-        str++;
-    }
-    return (1);
-}
-int multiply(int num1, int num2) {
-    return (num1 * num2);
-}
-
-int main(int argc, char *argv[])
+/**
+ * _puts - prints out a string that is followed by a new line
+ * @str: pointer to a string
+ * Return: Nothing
+*/
+void _puts(char *str)
 {
-	int num1, num2, result;
-    if (argc != 3) {
-        printf("Error\n");
-        return (98);
-    }
+	int i = 0;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+/**
+ * _atoi - converts string to an integer
+ * @s: pointer to string
+ * Return: integer
+ */
+int _atoi(const char *s)
+{
+	int si = 1;
+	unsigned long int result = 0, firstNum, a;
 
-    if (!isPositiveInteger(argv[1]) || !isPositiveInteger(argv[2])) {
-       printf("Error\n");
-        return (98);
-    }
+	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	{
+		if (s[firstNum] == '-')
+		{
+			si *= -1;
+		}
+	}
+	for (a= firstNum; s[a] >= 48 && s[a] <= 57; a++)
+	{
+		result *= 10;
+		result += (s[a] - 48);
+	}
+	return (si * result);
+}
+/**
+ * print_int - prints an integer.
+ * @n: int
+ * Return: 0
+ */
+void print_int(unsigned long int n)
+{
+	unsigned long int divisor = 1, i, r;
+	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+		;
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
+	{
+		r = n / divisor;
+		_putchar('0' + r);
+	}
+}
+/**
+ * main - prints the result of multiplication
+ * @argc: int
+ * @argv: list of arguments
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[2]);
-
-    result = multiply(num1, num2);
-
-    printf("%d\n", result);
-
-    return (0);
+	if (argc != 3)
+	{
+		_puts("Error ");
+		exit(98);
+	}
+	print_int(_atoi(argv[1]) * _atoi(argv[2]));
+	_putchar('\n');
+	return (0);
 }
